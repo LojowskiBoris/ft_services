694e32cc54ae5427855672d7ca71d0a5591c4eaf
blojowsk <blojowsk@student.le-101.fr>
2022-01-27 22:36:08 +0100

    really done

--- srcs/requirements/mariadb/tools/create_database.sh
+++ srcs/requirements/mariadb/tools/create_database.sh
@@ -6,22 +6,17 @@ if [ -d "/var/lib/mysql/$MYSQL_DB" ]
 then 
 	echo "Database already exists"
 else
-	mysql_secure_installation << EOF
 
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-Y
-Y
-Y
-EOF
+#mysql_secure_installation
+mysql -e "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';"
+mysql -e "DELETE FROM mysql.user WHERE User='';"
+#mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql -e "DROP DATABASE IF EXISTS test;"
+mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+mysql -e "FLUSH PRIVILEGES;"
 
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB; GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DB < /usr/local/bin/wordpress.sql
+mysql -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB; GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
+mysql $MYSQL_DB < /wordpress.sql
 
 fi
-
-service mysql stop
-
-exec "$@"
